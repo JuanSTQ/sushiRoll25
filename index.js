5bc5a96a6e3ec09e9de0f4c528c7f9632a72ddc9
juanstq <juanstq20@gmail.com>
2021-10-24 23:26:35 -0500

    text dinamimc

--- index.js
+++ index.js
@@ -16,3 +16,51 @@ setInterval(()=>{
     backend2.style.transform = "rotateY(0deg)"
   }
 },2700)
+
+/* ******** TEXT DINAMIC ******* */
+
+const underscore = document.getElementById('console')
+/* const ghost = document.getElementById('ghost-active') */
+const text = document.getElementById('text')
+
+
+
+const write = (word)=>{
+  const id = setInterval(()=>{
+    const lenghtText = text.textContent.length ||0
+    if(lenghtText===word.length){
+      clearInterval(id)
+      setTimeout(()=>{
+        eraser(word, 0)
+      }, 1200)
+      return true
+    }else{
+      text.textContent += word[lenghtText]
+    }
+  },80)
+}
+//CEO DevFolio,Web Developer,Web Designer,Frontend Developer,Graphic Designer
+const getWord = (beforeWord)=>{
+  const messages = ["INSCRIBETE 🍹", "ÚLTIMOS CUPOS", "🍹🍹🍹", "BIENVENIDO"];
+  const index=messages.indexOf(beforeWord)
+  if(index===messages.length-1){
+    return messages[0]
+  }
+  return messages[index+1]
+}
+const eraser = (word, count)=>{
+  let c = count;
+  const id = setInterval(()=>{
+    const lenghtText = text.textContent.length ||0
+    if(lenghtText===0){
+      clearInterval(id)
+      const newWord = getWord(word)
+      write(newWord)
+      return true
+    }
+    text.textContent = word.substring(0, word.length-c)
+    c++
+  },30)
+}
+write("BIENVENIDO")
+
